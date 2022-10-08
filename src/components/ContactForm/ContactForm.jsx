ef66f251a10bb67be61c4f0295c2a0acb7f43047
Viktoriia Mykhalova <mihaleva.victoria@gmail.com>
2022-10-08 17:46:37 +0300

    ContactForm refactored using React-hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,121 @@
-import { Component } from "react";
 import { nanoid } from 'nanoid';
 import { StyledForm, StyledLabel, StyledInput, StyledButton } from './ContactForm.styled';
 import PropTypes from 'prop-types';
+import { useState } from "react";
 
-class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''
+function ContactForm({ onSubmit }) {
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+
+    const handleChange = (event) => {
+        switch (event.target.name) {
+            case 'name':
+                return setName(event.target.value);
+            case 'number':
+                return setNumber(event.target.value);
+            default:
+                return;
+        }
     };
 
-    handleChange = (event) => {
-        this.setState({
-                [event.target.name]: event.target.value,             
-        })
-    }
-
-    handleSubmit = (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        this.props.onSubmit(this.state);
-
-        this.setState({
-            name: '',
-            number: ''
-        })
-
+        onSubmit({ 'name': name, 'number': number });
+        setName("");
+        setNumber("");
     }
-    
-    render() {
-        return (
-            <StyledForm onSubmit = {this.handleSubmit}>
-                <StyledLabel> Name
-                    <StyledInput
-                        id = {nanoid()}
-                        type="text"
-                        name="name"
-                        value={this.state.name}
-                        onChange = {this.handleChange}
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
+
+
+
+    return (
+        <StyledForm onSubmit = {handleSubmit}>
+            <StyledLabel> Name
+                <StyledInput
+                    id = {nanoid()}
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange = {handleChange}
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
+            </StyledLabel>
+            <StyledLabel> Number
+                <StyledInput
+                    type="tel"
+                    name="number"
+                    value={number}
+                    onChange= {handleChange}
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
                     />
-                </StyledLabel>
-                <StyledLabel> Number
-                    <StyledInput
-                        type="tel"
-                        name="number"
-                        value={this.state.number}
-                        onChange= {this.handleChange}
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                        />
-                </StyledLabel>
-                <StyledButton type="submit">Add contact</StyledButton>
-            </StyledForm>
-        )
-    }    
+            </StyledLabel>
+            <StyledButton type="submit">Add contact</StyledButton>
+        </StyledForm>
+    )   
 };
+
+
+
+
+
+
+// class ContactForm1 extends Component {
+//     state = {
+//         name: '',
+//         number: ''
+//     };
+
+//     handleChange = (event) => {
+//         this.setState({
+//                 [event.target.name]: event.target.value,             
+//         })
+//     }
+
+//     handleSubmit = (event) => {
+//         event.preventDefault();
+//         this.props.onSubmit(this.state);
+
+//         this.setState({
+//             name: '',
+//             number: ''
+//         })
+
+//     }
+    
+//     render() {
+//         return (
+//             <StyledForm onSubmit = {this.handleSubmit}>
+//                 <StyledLabel> Name
+//                     <StyledInput
+//                         id = {nanoid()}
+//                         type="text"
+//                         name="name"
+//                         value={this.state.name}
+//                         onChange = {this.handleChange}
+//                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//                         required
+//                     />
+//                 </StyledLabel>
+//                 <StyledLabel> Number
+//                     <StyledInput
+//                         type="tel"
+//                         name="number"
+//                         value={this.state.number}
+//                         onChange= {this.handleChange}
+//                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//                         required
+//                         />
+//                 </StyledLabel>
+//                 <StyledButton type="submit">Add contact</StyledButton>
+//             </StyledForm>
+//         )
+//     }    
+// };
 
 ContactForm.propTypes = {
     onSubmit: PropTypes.func.isRequired,
